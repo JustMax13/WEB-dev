ff54497193348eab57b1a545cbf55c0be6b2df1c
Max <khryshcheniukmaksym@gmail.com>
2023-05-02 17:58:43 +0300

    Консоль очікує

--- Lab7/Lab7/app.js
+++ Lab7/Lab7/app.js
@@ -1,3 +1,8 @@
 'use strict';
 
 console.log('Hello world');
+
+// код для того, щоб консоль автоматично не закривалася
+var readline = require('readline');
+var rl = readline.createInterface(process.stdin, process.stdout);
+// --
